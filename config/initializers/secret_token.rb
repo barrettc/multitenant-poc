3177f4b15cf1f6225a096ce80b10fab6fa6d9030
Barrett Cervenka <barrett@cervenka.com>
2013-01-19 13:20:56 -0600

    Just the barebones new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeGame::Application.config.secret_token = '4b9b6e0ae66645a68c8c0ec94f7b49d9df83dfe891fec0c948e9cb226997af6e2f937e42bed2220d509a521bdbf63cba6146482fa9051e4d8c6c83c6aebf4ef4'
